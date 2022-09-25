34d306336daf4dff3c7439d57e2b17cf55d672c6
Ben Jones <ben@bnjns.uk>
2022-09-25 20:48:40 +0100

    add tests for selecting the calendar method

--- src/calandars/index.ts
+++ src/calandars/index.ts
@@ -1,5 +1,5 @@
 import { getEnv } from '../config'
-import { CalendarTypeMap, CalendarEvent, CalendarType } from './types'
+import { CalendarTypeMap, CalendarEvent, CalendarType, CalendarFn } from './types'
 import { getEvents } from './google/events'
 import { DateTime } from 'luxon'
 import logger from '../utils/logging'
@@ -8,9 +8,9 @@ const calendarTypeMap: CalendarTypeMap = {
   'google': getEvents
 }
 
-export const getActiveEvents = async(calendarId: string): Promise<CalendarEvent[]> => {
+export const selectCalendarMethod = async(): Promise<CalendarFn> => {
   const allowedCalendars = Object.keys(calendarTypeMap)
-  const configuredCalendar = await getEnv('CALENDAR_TYPE', 'google')
+  const configuredCalendar = await getEnv('CALENDAR_TYPE', 'google') as CalendarType
 
   if (!allowedCalendars.includes(configuredCalendar)) {
     return Promise.reject(new Error(`Unrecognised calendar type '${configuredCalendar}' - must be one of ${allowedCalendars}`))
@@ -18,11 +18,16 @@ export const getActiveEvents = async(calendarId: string): Promise<CalendarEvent[
 
   logger.debug(`Selected calendar type: ${configuredCalendar}`)
 
-  const now = DateTime.now().setZone('UTC')
+  return Promise.resolve(calendarTypeMap[configuredCalendar])
+}
 
+
+export const getActiveEvents = async(calendarId: string): Promise<CalendarEvent[]> => {
+  const now = DateTime.now().setZone('UTC')
   logger.info(`Finding events that are active at ${now.toISO()}`)
 
-  return calendarTypeMap[configuredCalendar as CalendarType](now, calendarId)
+  return selectCalendarMethod()
+    .then(fn => fn(now, calendarId))
     .then(events => {
       logger.info(`Found ${events.length} active events`)
       return events
