68a8019ebfc35cdbe292872d25f6a86e3f7724b9
Ben Jones <ben@bnjns.uk>
2022-09-25 20:43:47 +0100

    add tests for selecting the secret method

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
-import { SecretType, SecretTypeMap } from './types'
+import { SecretFn, SecretType, SecretTypeMap } from './types'
 import getEnvVariable from './env'
 import logger from '../utils/logging'
 
@@ -6,16 +6,24 @@ const secretTypeMap: SecretTypeMap = {
   env:  getEnvVariable
 }
 
-export { default as getEnv } from './env'
-export const getSecret = async(name: string, defaultValue?: string): Promise<string> => {
-  const configuredSecretType = await getEnvVariable('SECRET_TYPE', 'env')
+export const selectSecretMethod = async(): Promise<SecretFn> => {
   const allowedSecretTypes = Object.keys(secretTypeMap)
+  const configuredSecretType = await getEnvVariable('SECRET_TYPE', 'env') as SecretType
 
   if (!allowedSecretTypes.includes(configuredSecretType)) {
     return Promise.reject(new Error(`Unrecognised secret type '${configuredSecretType}' - must be one of ${allowedSecretTypes}`))
   }
 
-  logger.debug(`Using type '${configuredSecretType}' to fetch secret ${name}`)
+  logger.debug(`Using secret type '${configuredSecretType}'`)
 
-  return secretTypeMap[configuredSecretType as SecretType](name, defaultValue)
+  return Promise.resolve(secretTypeMap[configuredSecretType])
+}
+
+export { default as getEnv } from './env'
+export const getSecret = async(name: string, defaultValue?: string): Promise<string> => {
+  return selectSecretMethod()
+    .then(fn => {
+      logger.debug(`Fetching secret ${name}`)
+      return fn(name, defaultValue)
+    })
 }
